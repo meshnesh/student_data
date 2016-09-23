0e8e8f307cefdab7af80aef9bcb5ba2c352aac71
franck <fkipchumba@intellisoftplus.com>
2016-09-23 14:02:23 +0300

    add data contract

---
+++ app/src/main/java/com/example/entsfrank/datacollection/DataContract.java
@@ -0,0 +1,66 @@
+package com.example.entsfrank.datacollection;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by frank on 9/23/16.
+ */
+public class DataContract {
+    private String _title, _description, _association, _purpose, _time, _reminder;
+    private int _id;
+
+    public DataContract(int id, String title, String description, String association, String purpose, String time, String reminder){
+        this._title = title;
+        this._description=description;
+        this._association =association;
+        this._purpose =purpose;
+        this._time =time;
+        this._id=id;
+        this._reminder=reminder;
+    }
+    public static abstract class DataEntry implements BaseColumns {
+        public static final String TABLE_NAME = "Calls";
+        public static final String COLUMN_TITLE = "title";
+        public static final String COLUMN_DESCRIPTION = "description";
+        public static final String COLUMN_ASSOCIATION = "association";
+        public static final String COLUMN_PURPOSE = "purpose";
+        public static final String COLUMN_TIME = "time";
+        public static final String COLUMN_REMINDER = "reminder";
+    }
+
+    public String getReminder() {
+        return _reminder;
+    }
+
+    public String getDescription() {
+        return _description;
+    }
+
+    public String getAssociation() {
+        return _association;
+    }
+
+    public String getPurpose() {
+        return _purpose;
+    }
+
+    public String getTitle() {
+        return _title;
+    }
+
+    public String getTime() {
+        return _time;
+    }
+
+    public int getId() {
+        return _id;
+    }
+
+    @Override
+    public String toString() {
+        return "AppointmentContract("+
+                "title="+_title+", description="+_description+
+                ", date="+ _time +", reminder="+_reminder+
+                ", who="+ _association +", why="+ _purpose +")";
+    }
+}
--- app/src/main/java/com/example/entsfrank/datacollection/DataListFragment.java
+++ app/src/main/java/com/example/entsfrank/datacollection/DataListFragment.java
@@ -3,10 +3,16 @@ package com.example.entsfrank.datacollection;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 
 /**
@@ -22,4 +28,36 @@ public class DataListFragment extends Fragment {
     public DataListFragment() {
         // Required empty public constructor
     }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_data_list, container, false);
+        RecyclerView recyclerView = (RecyclerView)view.findViewById(R.id.newRecycler);
+        FloatingActionButton floatingActionButton = (FloatingActionButton)view.findViewById(R.id.addBtn);
+        floatingActionButton.setOnClickListener(clickHandler);
+        List<DataContract> dataList;
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+        ReadCalls task = new ReadCalls(getActivity());
+        try{
+            callList = task.execute().get();
+            RecyclerView.Adapter adapter = new CallListAdapter(callList, getActivity());
+            recyclerView.setAdapter(adapter);
+        } catch (InterruptedException|ExecutionException e){
+            e.printStackTrace();
+        }
+        return view;
+    }
+    View.OnClickListener clickHandler = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            getFragmentManager()
+                    .beginTransaction()
+                    .addToBackStack(null)
+                    .replace(R.id.callContainer, new CreateCallFragment())
+                    .commit();
+        }
+    };
 }
