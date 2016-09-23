3b4cb807d3fa0f89f2036883476768cdda3cd5c7
franck <fkipchumba@intellisoftplus.com>
2016-09-23 13:49:29 +0300

    add datalist fragment

---
+++ app/src/main/java/com/example/entsfrank/datacollection/DataListFragment.java
@@ -0,0 +1,25 @@
+package com.example.entsfrank.datacollection;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link DataListFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link DataListFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class DataListFragment extends Fragment {
+
+    public DataListFragment() {
+        // Required empty public constructor
+    }
+}
